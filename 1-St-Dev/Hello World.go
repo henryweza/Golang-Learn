ae7ccd353365ef04544fb639a4dadc1d666acfa9
henryweza <henrywezuza@gmail.com>
2019-07-04 15:31:25 +0700

    do great 2

--- 1-St-Dev/Hello World.go
+++ 1-St-Dev/Hello World.go
@@ -1,1 +1,9 @@
-package __St_Dev
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	fmt.Println("Hello World")
+}
--- 2-nd-Dev/Hello World-2.go
+++ 2-nd-Dev/Hello World-2.go
@@ -1,1 +1,34 @@
-package __nd_Dev
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Have fun for fucking thing inthis training")
+	foo() //function foo
+	fmt.Println("Make This More")
+
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Println("this a ODD number =",i)
+			//fmt.Printf("|%-6s|%-6s|\n",i,"this a ODD number")
+		}
+		if i%2 != 0 {
+			fmt.Println("this Not ODD number =",i)
+			//fmt.Printf("|%-6s|%-6s|\n",i,"this Not ODD number")
+		}
+
+	}
+	bar() // bar function
+}
+
+func foo() {
+	fmt.Println("Test the function Foo")
+}
+
+func bar() {
+	fmt.Println("If Failure you will control flow")
+}
+//control flow:
+// (1) sequence
+// (2) loop interactive
+// (3) condition
